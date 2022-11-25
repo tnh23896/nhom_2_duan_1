85a0445770cfeef78623c59a9df16b55870cfe75
Duonglor2k3 <97235105+Duonglor2k3@users.noreply.github.com>
2022-11-25 18:02:43 +0700

    mc

--- modules/client/login/controllers/indexController.php
+++ modules/client/login/controllers/indexController.php
@@ -1,11 +1,39 @@
 <?php
 
-function construct()
-{
+function construct() {
     load_model('index');
 }
 
-function indexAction()
-{
-    load_view('index');
+function indexAction() {
+    request_login(false);
+    $notifications = get_notification();
+    load_view('index', [
+        "notifications" => $notifications
+    ]);
+}
+
+function indexPostAction() {
+    request_login(false);
+    // validation
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if (empty($username) || empty($password)) {
+        push_notification('red', ['Vui lòng nhập đầy đủ thông tin tài khoản và mật khẩu']);
+        header('Location: ?role=client&mod=login');
+    }
+    // xử lý đăng nhập
+    $login = get_login_user($username, $password);
+    if ($login) {
+        push_login($login);
+        header('Location: ?role=client');
+    } else {
+        push_notification('red', ['Tài khoản hoặc mật khẩu không chính xác']);
+        header('Location: ?role=client&mod=login');
+    }
+}
+
+function logoutAction() {
+    request_login();
+    remove_login();
+    header("Location: ?role=client&mod=login");
 }
---
+++ modules/client/login/models/indexModel.php
@@ -0,0 +1,7 @@
+<?php
+
+function get_login_user($username, $pass)
+{
+    $result = db_fetch_row("SELECT * FROM `users` WHERE `email` = '$username' AND `password` = '$pass' AND `role` = 2");
+    return $result;
+}
