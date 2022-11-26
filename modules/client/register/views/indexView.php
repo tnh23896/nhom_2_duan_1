23a8ac54b4a6a4f65721764187790d35a6ef9cbc
Duonglor2k3 <97235105+Duonglor2k3@users.noreply.github.com>
2022-11-26 14:32:27 +0700

    PO

--- modules/client/register/views/indexView.php
+++ modules/client/register/views/indexView.php
@@ -23,24 +23,24 @@
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="Email">
                     Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Email" type="text" placeholder="Email">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Email" type="text" placeholder="Email" name="email" >
             </div>
             <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="pass">
                     Mật Khẩu
                 </label>
-                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="pass" name="pass"  type="password" placeholder="******************">
                 <p class="text-red-500 text-xs italic">Mật khẩu không khớp</p>
             </div>
             <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="pass">
                     Nhập Lại Mật Khẩu
                 </label>
-                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="re_pass" name="re_pass" type="password"  placeholder="******************">
                 <p class="text-red-500 text-xs italic">Mật khẩu không khớp</p>
             </div>
             <div class="flex items-center justify-between">
-                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Đăng ký
                 </button>
             </div>
