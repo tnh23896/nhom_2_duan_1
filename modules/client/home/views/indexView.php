2fb82252ab0389e2ca7712e860229c9d99a6cd1c
duyynguyenn <114343968+duyynguyenn@users.noreply.github.com>
2022-11-30 17:26:42 +0700

    Update indexView.php

--- modules/client/home/views/indexView.php
+++ modules/client/home/views/indexView.php
@@ -11,16 +11,21 @@
         <!-- <img src="public/images/banner.png" alt="" class="w-full"> -->
         <div class="slideshow-container">
             <div class="mySlides fade">
-                <img src="public/images/banner.png" class="w-full">
+                <img src="public/images/banner.png" class="w-full h-[500px]">
             </div>
             <div class="mySlides fade">
-                <img src="public/images/thichsua.jpg" class="w-full">
+                <img src="public/images/thichsua.jpg" class="w-full h-[500px]">
             </div>
             <div class="mySlides fade">
-                <img src="public/images/banner3.jpg" class="w-full">
+                <img src="public/images/banner3.jpg" class="w-full h-[500px]">
             </div>
             <a class="prev" onclick="plusSlides(-1)">❮</a>
             <a class="next" onclick="plusSlides(1)">❯</a>
+        </div>
+        <div style="text-align:center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
         </div>
     </section>
     <section class="bg-[#F9F9F9] text-[15px] flex justify-center space-x-[75px] mb-[21px]">
@@ -108,20 +113,21 @@
         showSlides(slideIndex = n);
     }
 
-    function showSlides(n) {
+    function showSlides() {
         let i;
         let slides = document.getElementsByClassName("mySlides");
         let dots = document.getElementsByClassName("dot");
-        if (n > slides.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = slides.length }
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "");
         }
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
     }
 </script>
 <?php get_footer('') ?>
